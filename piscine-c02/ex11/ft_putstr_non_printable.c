ba060e174204c37703d2455cc7e4449ee3aab709
Franck Michael Fierro Chicaiza <ffierro-@c3r10s4.42madrid.com>
2024-07-13 13:29:34 +0200

    fixed c02

--- piscine-c02/ex11/ft_putstr_non_printable.c
+++ piscine-c02/ex11/ft_putstr_non_printable.c
@@ -6,39 +6,20 @@
 /*   By: ffierro- <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/10 16:51:40 by ffierro-          #+#    #+#             */
-/*   Updated: 2024/07/10 19:24:15 by ffierro-         ###   ########.fr       */
+/*   Updated: 2024/07/13 13:27:05 by ffierro-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
-//#include <stdio.h>
 
 void	write_hex(char car)
 {
-	int		a;
 	char	*c;
 
 	c = "0123456789abcdef";
 	write(1, "\\", 1);
-	a = car;
-	if (a < 16)
-	{
-		write(1, "0", 1);
-		if (a < 10)
-			write(1, &c[a], 1);
-		else
-			write(1, &c[a], 1);
-	}
-	else if (a < 32)
-	{
-		write(1, "1", 1);
-		if (a - 16 < 10)
-			write(1, &c[a], 1);
-		else
-			write(1, &c[a], 1);
-	}
-	else if (car == 127)
-		write(1, "7f", 2);
+	write(1, &c[car / 16], 1);
+	write(1, &c[car % 16], 1);
 }
 
 void	ft_putstr_non_printable(char *str)
@@ -51,9 +32,7 @@ void	ft_putstr_non_printable(char *str)
 		if (str[i] >= 32 && str[i] <= 126)
 			write(1, &str[i], 1);
 		else
-		{
 			write_hex(str[i]);
-		}
 		i++;
 	}
 }
